e24d882a03c1f13083cbbe32008af111ac7dcea8
annieyl <yuqi.annie.liu@gmail.com>
2025-04-05 16:00:39 -0500

    added basic functionalities for homepage, video upload, and logs

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+import VideoUpload from '../components/VideoUpload';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <VideoUpload />
+    </div>
+  );
+};
+
+export default Home;
